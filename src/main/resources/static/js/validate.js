650a77100a8ea1054dce801fb5e7f7fbcff2ebad
Luke6933 <secondary.bonanno@gmail.com>
2023-10-26 12:15:09 +0200

    first js

---
+++ src/main/resources/static/js/validate.js
@@ -0,0 +1,10 @@
+function verificaRistorante(){
+    var nomeRistTxt = document.getElementById("nome_rist");
+    var descRistTxt = document.getElementById("descr_rist");
+    var locRistTxt = document.getElementById("ubc");
+
+    alert(nomeRistTxt.value);
+    alert(descRistTxt.value);
+    alert(locRistTxt.value);
+
+}
