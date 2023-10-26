13da45d09a7d300a9f3a0f82cc1d75f107b9c2cd
Luke6933 <secondary.bonanno@gmail.com>
2023-10-26 11:19:56 +0200

    Initial commit

---
+++ src/test/java/it/unical/informatica/webapp24/recensioneristoranti/RecensioneRistorantiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.informatica.webapp24.recensioneristoranti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecensioneRistorantiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
